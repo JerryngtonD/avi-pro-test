5558a4cae52e10e1c5d267217047c350d4c31ed9
jerryngton <jerryngtond@gmail.com>
2019-03-29 10:53:30 +0300

    handlers on main view page

--- www/hello.dev/index.php
+++ www/hello.dev/index.php
@@ -1,53 +1,8 @@
 <?php
-$url_info = parse_url($_SERVER['REQUEST_URI']);
-
-function getGUID()
-{
-    if (function_exists('com_create_guid')) {
-        return com_create_guid();
-    } else {
-        mt_srand((double)microtime()*10000);
-        $charid = strtoupper(md5(uniqid(rand(), true)));
-        $hyphen = chr(45);// "-"
-        $uuid = substr($charid, 0, 8)
-            .substr($charid, 8, 4)
-            .substr($charid, 12, 4)
-            .substr($charid, 16, 4)
-            .substr($charid, 20, 12);
-
-        return $uuid;
-    }
-}
-
-function getIdOnQuery()
-{
-    $required_type = $_POST['type'];
-    $required_length = $_POST['length'];
-    $output_value = uniqid();
-
-    if ($required_type == 'string') {
-        $output_value = strtolower(preg_replace('/[0-9_\/]+/', '', base64_encode(sha1($output[id]))));
-    } elseif ($required_type == 'digit') {
-        $output_value = crc32($output_value);
-    } elseif ($required_type == 'guid') {
-        $output_value = getGUID();
-    }
-
-    if (empty($required_length) or !is_numeric($required_length)) {
-        return $output_value;
-    } elseif (is_numeric($required_length)) {
-        return mb_strimwidth($output_value, 0, $required_length);
-    }
-}
-
-function getValueOnId($id)
-{
-    return sha1($id);
-}
+  include_once(__DIR__ . '/Store.php');
 ?>
 
-<!DOCTYPE html>
-<html lang="ru">
+<html>
 <head>
     <meta charset="UTF-8">
     <title>Главная</title>
@@ -93,16 +48,11 @@ function getValueOnId($id)
       </p>
     </form>
 
-    <?php if ($url_info['path'] == '/api/generate/'):?>
-      <h3>
-        Твой уникальный id: <?= getIdOnQuery()?>
-      </h3>
-
-    <?php elseif ($url_info['path'] == '/api/retrieve' and isset($url_info['query']) and !empty($_GET['id'])) :?>
-      <?php $sesion_id = $_GET['id']; ?>
-      <h3> Твой уникальный id:</h3><?= $sesion_id ?>
-      <h3> Значение по id:</h3> <?= getValueOnId($sesion_id)?>
-    <?php endif;?>
+    <?php
+      echo $_SESSION['router']->handleGetId();
+      echo $_SESSION['router']->handleGetValueOnId();
+    ?>
+    
 </div>
 </body>
 </html>
